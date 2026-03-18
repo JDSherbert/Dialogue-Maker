f0c69a2d93ccdb759ca1c08f5309a85bf71f525f
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2026-03-18 12:52:17 +0000

    Update Languages.js

--- Modules/Localization/Languages.js
+++ Modules/Localization/Languages.js
@@ -1,1 +1,9 @@
+// Copyright (c) 2026 JDSherbert. All rights reserved.
 
+export const Languages = [
+    { code: "ENG", name: "English" },
+    { code: "JP", name: "Japanese" },
+    { code: "FR", name: "French" },
+    { code: "DE", name: "German" },
+    { code: "ES", name: "Spanish" }
+];
